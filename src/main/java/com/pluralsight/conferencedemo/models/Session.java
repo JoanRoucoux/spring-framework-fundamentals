f764c56bb10adf9a47e2c8642d4d18788d601b7f
“JoanRoucoux” <joan.roucoux@gmail.com>
2020-09-02 19:48:27 +0200

    [EVO] Add crud endpoints and models

---
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -0,0 +1,40 @@
+package com.pluralsight.conferencedemo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity(name = "sessions")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+@Getter
+@Setter
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+
+    private String session_name;
+    private String session_description;
+    private Integer session_length;
+
+    @ManyToMany
+    @JoinTable(
+            name = "session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id")
+    )
+    private List<Speaker> speakers;
+
+}
---
+++ src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -0,0 +1,43 @@
+package com.pluralsight.conferencedemo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+@Getter
+@Setter
+public class Speaker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long speaker_id;
+
+    private String first_name;
+    private String last_name;
+    private String title;
+    private String company;
+    private String speaker_bio;
+
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
+
+    @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
+    private List<Session> sessions;
+
+}
